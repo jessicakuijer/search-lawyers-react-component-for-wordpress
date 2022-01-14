b6f3c1bcbe25f2f0b1f414d8581c6c5d8f70d07c
jessica kuijer <6564174-jessicakuijer@users.noreply.gitlab.com>
2022-01-14 09:33:03 +0100

    presave-changement-interface

--- v4/js/search-lawyers.js
+++ v4/js/search-lawyers.js
@@ -203,7 +203,7 @@ class SearchLawyers extends React.Component {
    */
    async getSpecialities() {
       let myHeaders = new Headers({
-         'Authorization': 'Bearer ' + ENV_TOKENJESS,
+         'Authorization': 'Bearer d68406ad89cf138517e85a0cb1d25589:6d27a26c0f1cb45e4541088de4abd7ba76e44b784ea11519070a917dbe2ad24d730a45d571041ad5e115bd2f5e385188ae551d26e35a588f574056fecab35733912948795001ed5a4900f935ec7a9a20da07a8861e74a11b01687d2ac81232c29343bc018b14e0fbd8d9d2a809684160',
          'Content-Type': 'application/json'
       });
       // make api call to get lawyers array
@@ -234,7 +234,7 @@ class SearchLawyers extends React.Component {
    async onSearchGetLawyers(userSearchStr) {
       // Configurer le headers de la requête à envoyer
       let myHeaders = new Headers({
-         'Authorization': 'Bearer ' + ENV_TOKENJESS,
+         'Authorization': 'Bearer d68406ad89cf138517e85a0cb1d25589:6d27a26c0f1cb45e4541088de4abd7ba76e44b784ea11519070a917dbe2ad24d730a45d571041ad5e115bd2f5e385188ae551d26e35a588f574056fecab35733912948795001ed5a4900f935ec7a9a20da07a8861e74a11b01687d2ac81232c29343bc018b14e0fbd8d9d2a809684160',
          'Content-Type': 'application/json'
       });
       // make api call to get lawyers array
